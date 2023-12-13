9237387f65fbc8254f1ff58ed7a8775439430fc0
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2023-12-12 23:07:19 -0500

    Revert "change env var name"
    This reverts commit ceda58f12400814b7d75d20d5c5c12baa67030eb.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 from random import randint, random
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
@@ -13,6 +14,7 @@ from app.api.router import router
 from app.db.schema import Category, Product
 
 app = FastAPI()
+load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,7 +28,7 @@ register_tortoise(
     app,
     config={
         "connections": {
-            "default": expand_db_url(str(os.environ["DATABASE_PRIVATE_URL"]), "asyncpg")
+            "default": expand_db_url(str(os.environ["POSTGRES_URL"]), "asyncpg")
         },
         "apps": {
             "models": {
